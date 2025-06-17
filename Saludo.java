8f40b6d74c6fae4f3f57359cc1a58731a3eada6c
camipaula <103073699+camipaula@users.noreply.github.com>
2025-06-17 02:30:39 +0000

    Corregir error en saludo

--- Saludo.java
+++ Saludo.java
@@ -1,7 +1,7 @@
 public class Saludo {
     public String generarSaludo(String nombre) {
         if (nombre == null | | nombre. isEmpty() ) {
-        return "iHola, Invitado!";
+            return "iHola, Invitado!";
         }
         return "iHola, " + nombre + "!";
 }
